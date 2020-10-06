9502a3e38fd11b0c6997ff4413de98b470ef0ad7
Juan <sanchezsjuan07@gmail.com>
2020-10-06 17:28:55 -0300

    setup.sh actualizado

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 apt install python3 python3-pip
-pip3 install python-magic
-pip3 install python-axolotl
-pip3 install selenium
-pip3 install openwa
+pip3 install python-magic Pillow python-axolotl selenium openwa requests
